48e168f7f4f380090e02ce3b498e50e775137cb4
Gurunath <bhuvi23389@gmail.com>
2022-04-27 20:25:06 +0100

    Installed msw and Added function to display the first film name from ghibliapi.herokuapp.com/films

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+    const [film, setFilm] = useState<string>("");
+
+    useEffect(() => {
+        async function getFirstFilm() {
+            const response = await fetch(
+                `https://ghibliapi.herokuapp.com/films`
+            );
+            let result = await response.json();
+            setFilm(result[0].title);
+        }
+        getFirstFilm();
+    }, []);
     return (
         <div className="App">
             <header className="App-header">Studio Ghibli API</header>
+            <p>{film}</p>
         </div>
     );
 }
